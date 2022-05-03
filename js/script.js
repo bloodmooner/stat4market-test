2a72ff2491b66dd32fbc557f5765fa315f132edc
Sergey Goncharuk <fleshed914@gmail.com>
2022-05-04 01:07:22 +0300

    feat: add anchor to study section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 
 $(document).ready(function(){
 
-   
+  $('a[href^="#"').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top - 100
+    });
+    return false;
+  });
 
 });
 
